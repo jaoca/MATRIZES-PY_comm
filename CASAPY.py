a41d5419c2219d8df3de80970201be17e587b98e
jaoca <115593555+jaoca@users.noreply.github.com>
2022-11-22 17:40:15 -0300

    conserto da casinha
    casapy tem comentarios vestigiais do monitor, substitui com comentarios mais relevantes.
    alem disso, ele nao conta para o somatorio a seçao triangulo

--- CASAPY.py
+++ CASAPY.py
@@ -1,29 +1,29 @@
 def crie_matriz(n_linhas, n_colunas, valor):
     ''' (int, int, valor) -> matriz
-
     Cria e retorna uma matriz com n_linhas e n_colunas
     em que cada elemento dentro do desenho é igual ao valor.
     '''
     aresta=24 #tamanho das arestas do quadrado,min 3
     linha=4 #começo da linha do quadrado
     coluna=5 #começo da coluna do quadrado
-    altura=12
+    altura=12 #altura do triangulo
     inicio=10 #inicio da forma
     maxc=aresta+inicio #coluna maxima
-    maxt=inicio+(aresta/2)
-    maxa=aresta+altura
+    maxt=inicio+(aresta/2) #area maxima do triangulo
+    maxa=aresta+altura #tamanho da forma
     #i=coluna atual
     #j=linha atual
     adicao=0 #variavel para o somatorio
     matriz = [] # lista vazia
     for i in range(n_linhas): 
         linha = [] 
-        for j in range(n_colunas): #espaço sem desenho
+        for j in range(n_colunas):
             if i < altura: 
-                if ((j+i<maxt) or (j-i>maxt+1)):
+                if ((j+i<maxt) or (j-i>maxt+1)): #espaço sem desenho
                     linha.append(0)
                 else:
-                    linha.append(1)
+                    linha.append(valor)
+                    adicao=adicao+valor
             elif i>=altura and i < maxa: #quadrado
                 if (j <= inicio) or (j > maxc):
                     linha.append(0) #fora do desenho
